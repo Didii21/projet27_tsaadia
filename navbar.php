7cfd4b6f12267d1b2979b71330ddec32267e377b
Didii21 <140284548+Didii21@users.noreply.github.com>
2023-12-02 15:18:11 +0100

    fin

---
+++ navbar.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+<script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+<ul class="nav justify-content-center" style:"">
+  <li class="nav-item">
+    <a class="nav-item" aria-current="page" href="index.php">Accueil</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-item" href="dedougou.php">Dedougou</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-item" href="ouagadougou.php">Ouagadougou</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-item" aria-disabled="false">Banfora</a>
+  </li>
+</ul> 
+</body>
+</html>
